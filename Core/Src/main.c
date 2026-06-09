f0e50e639b58c08187decb89c8c568806048f4cf
a1234 <9raya02@gmail.com>
2026-06-09 22:55:10 +0200

    add what i need to add

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,6 +23,10 @@
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include <string.h>
+//defineciones
+#define MODER_PC ((3U << (0 * 2)) | (3U << (1 * 2)) | (3U << (2 * 2))| (3U << (3 * 2)) | (3U << (4 * 2)) | (3U << (5 * 2)) | (3U << (6 * 2)) | (3U << (7 * 2))| (3U << (8 * 2)) | (3U << (9 * 2)))
+#define PUPDR_PC ((2U << (0 * 2)) | (2U << (1 * 2)) | (2U << (2 * 2))| (2U << (3 * 2)) | (2U << (4 * 2)) | (2U << (5 * 2)) | (2U << (6 * 2)) | (2U << (7 * 2))| (2U << (8 * 2)) | (2U << (9 * 2)))
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -58,6 +62,49 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void botton_init(){
+	//0.abelertsr el reloj de PAy PB, PC
+		RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN | RCC_AHB2ENR_GPIOCEN);      // Reloj para PA1, PA4 y PA5, PB5, y los de PC
+
+	// 1.Configurar PA1, 5, 4 y PB4 como Entrada con resistencia Pull-Down
+	    GPIOA->MODER &= ~((3U << (1 * 2)) | (3U << (4 * 2)) | (3U << (5 * 2))); // Modo Entrada (00) PA
+		GPIOA->PUPDR &= ~((3U << (1 * 2)) | (3U << (4 * 2)) | (3U << (5 * 2))); // Limpiar configuración previa
+		GPIOA->PUPDR |=  ((2U << (1 * 2)) | (2U << (4 * 2)) | (2U << (5 * 2))); // Activar resistencia Pull-Down (0)
+
+	    GPIOB->MODER &= ~(3U << (5 * 2));         // Modo Entrada (00) para PB4
+		GPIOB->PUPDR &= ~(3U << (5 * 2));         // Limpiar configuración previa
+		GPIOB->PUPDR |=  (2U << (5 * 2));         // Activar resistencia Pull-Down (0)
+	//2. Configuracion PC0 hasta PC9 como entradas PULL-Down
+		GPIOC->MODER &= ~(MODER_PC); // Modo Entrada (00) PA
+		GPIOC->PUPDR &= ~(MODER_PC); // Limpiar configuración previa
+		GPIOC->PUPDR |=  (PUPDR_PC); // Activar resistencia Pull-Down (0)
+
+}
+// Función que escanea todos los botones del volante
+uint8_t Leer_Botones(void) {
+
+	//--- Los 4 Botones Traseras ---
+		if ((GPIOA->IDR & (1 << 1)) != 0) return 1; // PA1 -> Botón 1
+		if ((GPIOA->IDR & (1 << 4)) != 0) return 2; // PA4 -> Botón 2
+		if ((GPIOA->IDR & (1 << 5)) != 0) return 3; // PA5 -> Botón 3
+		if ((GPIOB->IDR & (1 << 5)) != 0) return 4; // PB4 -> Botón 4
+
+    // --- LOS 5 DE LA IZQUIERDA (PC0 a PC4) ---
+        if ((GPIOC->IDR & (1 << 0)) != 0) return 5; // PC0
+        if ((GPIOC->IDR & (1 << 1)) != 0) return 6; // PC1
+        if ((GPIOC->IDR & (1 << 2)) != 0) return 7; // PC2
+        if ((GPIOC->IDR & (1 << 3)) != 0) return 8; // PC3
+        if ((GPIOC->IDR & (1 << 4)) != 0) return 9; // PC4
+
+    // --- LOS 5 DE LA DERECHA (PC5 a PC9) ---
+        if ((GPIOC->IDR & (1 << 5)) != 0) return 10; // PC5
+        if ((GPIOC->IDR & (1 << 6)) != 0) return 11; // PC6
+        if ((GPIOC->IDR & (1 << 7)) != 0) return 12; // PC7
+        if ((GPIOC->IDR & (1 << 8)) != 0) return 13; // PC8
+        if ((GPIOC->IDR & (1 << 9)) != 0) return 14; // PC9
+
+    return 0; // Si llega aquí, es que no hay nada pulsado
+}
 
 /* USER CODE END 0 */
 
@@ -71,7 +118,7 @@ int main(void)
   /* USER CODE BEGIN 1 */
 	uint8_t current_state[4] = {1, 1, 1, 1};
 	uint8_t previous_state[4] = {1, 1, 1, 1};
-	char tx_buffer[100];
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -93,14 +140,15 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-
+  	botton_init(); // <--- IMPORTANTE: Inicializa los 14 pines
+    printf("--- DEBUG DE 14 BOTONES HELIOS INICIADO ---\r\n");
   /* USER CODE END 2 */
 
   /* Initialize led */
-  BSP_LED_Init(LED_GREEN);
+  //BSP_LED_Init(LED_GREEN);
 
   /* Initialize USER push-button, will be used to trigger an interrupt each time it's pressed.*/
-  BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
+  // BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
 
   /* Initialize COM1 port (115200, 8 bits (7-bit data + 1 stop bit), no parity */
   BspCOMInit.BaudRate   = 115200;
@@ -117,9 +165,9 @@ int main(void)
 
   /* -- Sample board code to send message over COM1 port ---- */
   printf("Welcome to STM32 world !\n\r");
-
+  int boton_anterior = 0;
   /* -- Sample board code to switch on led ---- */
-  BSP_LED_On(LED_GREEN);
+ /* BSP_LED_On(LED_GREEN);*/
 
   /* USER CODE END BSP */
 
@@ -129,18 +177,18 @@ int main(void)
   {
 
     /* -- Sample board code for User push-button in interrupt mode ---- */
-    if (BspButtonState == BUTTON_PRESSED)
+   /* if (BspButtonState == BUTTON_PRESSED)
     {
       /* Update button state */
-      BspButtonState = BUTTON_RELEASED;
+      //BspButtonState = BUTTON_RELEASED;
       /* -- Sample board code to toggle led ---- */
-      BSP_LED_Toggle(LED_GREEN);
+      //BSP_LED_Toggle(LED_GREEN);
 
       /* ..... Perform your action ..... */
-    }
+    //}
     /* USER CODE END WHILE */
     /* USER CODE BEGIN 3 */
-    // 1. Leer los 4 pines
+  /*  // 1. Leer los 4 pines
         current_state[0] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1); // PA1
         current_state[1] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4); // PA4
         current_state[2] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5); // PA5
@@ -168,7 +216,7 @@ int main(void)
     // 3. Guardar el estado actual
       for (int i = 0; i < 4; i++) {
           previous_state[i] = current_state[i];
-      }
+      }*/
       /*
         if (current_state[0] == previous_state[0] ||
             current_state[1] == previous_state[1] ||
@@ -184,10 +232,41 @@ int main(void)
             previous_state[1] = current_state[1];
             previous_state[2] = current_state[2];
             previous_state[3] = current_state[3];
-        }*/
+        }
 
         // 5. Pequeña pausa para evitar rebotes eléctricos del botón
-        HAL_Delay(1000);
+        HAL_Delay(10);*/
+
+  // 1. Escaneamos todos los botones (PA1-5, PB5, PC0-9)
+      int boton_actual = Leer_Botones();
+
+      // 2. Si el botón que leo es diferente al que leí antes...
+      if (boton_actual != boton_anterior)
+      {
+        if (boton_actual != 0)
+        {
+          // Se ha pulsado un botón
+          printf("BOTON PULSADO: %d ", boton_actual);
+
+          // Debug específico para que sepas qué pin es en el terminal
+          if (boton_actual <= 3) printf("(Puerto A)\r\n");
+          else if (boton_actual == 4) printf("(Puerto B)\r\n");
+          else if (boton_actual <= 9) printf("(Puerto C IZQ)\r\n");
+          else printf("(Puerto C DER)\r\n");
+        }
+        else
+        {
+          // Se ha soltado el botón
+          printf("BOTON SOLTADO (Ninguno activo)\r\n");
+        }
+
+        // 3. Actualizamos el estado anterior
+        boton_anterior = boton_actual;
+      }
+
+      // 4. Pausa de 20ms para el "Debounce" (evita que el ruido eléctrico mande 100 mensajes)
+      HAL_Delay(20);
+
   }
   /* USER CODE END 3 */
 }
